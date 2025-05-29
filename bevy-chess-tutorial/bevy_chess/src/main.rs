caaa638055f9e931a91f733a731ac086f507ec0f
milliorn <scottmilliorn@gmail.com>
2025-05-29 00:03:43 -0700

    feat: spawn 8x8 ground plane using Rectangle and StandardMaterial"

--- bevy-chess-tutorial/bevy_chess/src/main.rs
+++ bevy-chess-tutorial/bevy_chess/src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_math::primitives::Rectangle;
 
 fn main() {
     App::new()
@@ -11,5 +12,22 @@ fn main() {
             }),
             ..default()
         }))
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(Rectangle::new(8.0, 8.0))),
+        material: materials.add(StandardMaterial {
+            base_color: Color::rgb(1., 0.9, 0.9),
+            ..default()
+        }),
+        transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
+        ..Default::default()
+    });
 }
