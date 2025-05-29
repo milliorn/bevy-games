0976376b045c3d958f129ba0441724f7f1e2f349
milliorn <scottmilliorn@gmail.com>
2025-05-28 23:07:17 -0700

    feat: initialize Bevy app with MSAA and window settings for "Rusty Chess"

--- bevy-chess-tutorial/bevy_chess/src/main.rs
+++ bevy-chess-tutorial/bevy_chess/src/main.rs
@@ -1,5 +1,15 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(Msaa::default())
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Rusty Chess".to_string(),
+                resolution: (1400., 1400.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
