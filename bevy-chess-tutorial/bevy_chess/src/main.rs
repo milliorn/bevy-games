085de4fe13af962d25e6ed86840fa204f9de8c4e
milliorn <scottmilliorn@gmail.com>
2025-05-30 00:24:24 -0700

    feat: initialize Bevy app with plane, camera, and light using latest API

--- bevy-chess-tutorial/bevy_chess/src/main.rs
+++ bevy-chess-tutorial/bevy_chess/src/main.rs
@@ -1,12 +1,13 @@
-use bevy::{math::primitives::Rectangle, prelude::*};
+use bevy::math::primitives::Plane3d;
+use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .insert_resource(Msaa::default())
+        .insert_resource(Msaa::default()) // MSAA 4x by default
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Rusty Chess".to_string(),
-                resolution: (1400., 1400.).into(),
+                resolution: (1600., 1600.).into(),
                 ..default()
             }),
             ..default()
@@ -20,22 +21,30 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // Plane
     commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(Rectangle::new(8.0, 8.0))),
+        mesh: meshes.add(Mesh::from(Plane3d::default())),
         material: materials.add(StandardMaterial {
             base_color: Color::rgb(1., 0.9, 0.9),
             ..default()
         }),
-        transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)), // Centered at origin
         ..default()
     });
 
+    // Camera
     commands.spawn(Camera3dBundle {
         transform: Transform {
             translation: Vec3::new(-7.0, 20.0, 4.0),
             rotation: Quat::from_euler(EulerRot::XYZ, -0.3, -0.5, -0.3),
             ..default()
         },
+        ..default()
+    });
+
+    // Light
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
         ..default()
     });
 }
