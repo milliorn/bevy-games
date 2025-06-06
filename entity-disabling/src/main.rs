c038391696936d7c9644c457996a44f0747fa9f0
milliorn <scottmilliorn@gmail.com>
2025-06-06 15:43:50 -0700

    Add the DisableOnClick System and Component

--- entity-disabling/src/main.rs
+++ entity-disabling/src/main.rs
@@ -1,7 +1,23 @@
-use bevy::prelude::*;
+use bevy::{ecs::entity_disabling::Disabled, prelude::*};
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, MeshPickingPlugin))
+        .add_observer(disable_entities_on_click)
         .run();
+}
+
+#[derive(Component)]
+struct DisableOnClick;
+
+fn disable_entities_on_click(
+    trigger: Trigger<Pointer<Click>>,
+    valid_query: Query<&DisableOnClick>,
+    mut commands: Commands,
+) {
+    let clicked_entity = trigger.target();
+
+    if valid_query.contains(clicked_entity) {
+        commands.entity(clicked_entity).insert(Disabled);
+    }
 }
