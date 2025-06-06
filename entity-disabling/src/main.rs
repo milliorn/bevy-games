7ffc323b5318f3e75b9d157dfba9991d6d0b2a18
milliorn <scottmilliorn@gmail.com>
2025-06-06 13:32:26 -0700

    nitialize Bevy app with DefaultPlugins

--- entity-disabling/src/main.rs
+++ entity-disabling/src/main.rs
@@ -1,3 +1,7 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new() // create a new bevy app
+        .add_plugins(DefaultPlugins) // adds default set of plugins from bevy
+        .run(); // starts app
 }
