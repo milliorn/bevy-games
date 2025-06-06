09c2161f6b013bbf10f26351a79e359079aa2b06
milliorn <scottmilliorn@gmail.com>
2025-06-06 16:37:40 -0700

    Add the DisableOnClick System and Component

--- entity-disabling/src/main.rs
+++ entity-disabling/src/main.rs
@@ -1,23 +1,34 @@
+// import bevy core
+// import Disabled component.
+// Disabled when added to an entity removes it from most system queries automatically (not skipped)
 use bevy::{ecs::entity_disabling::Disabled, prelude::*};
 
 fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins, MeshPickingPlugin))
-        .add_observer(disable_entities_on_click)
-        .run();
+    // point of entry
+    App::new() //create a new bevy app
+        .add_plugins((DefaultPlugins, MeshPickingPlugin)) //click on meshes
+        .add_observer(disable_entities_on_click) // runs only when click event is detected
+        .run(); // begin game loop
 }
 
+// Define a new component, DisabledOnClick
+// This is a marker component, it doesn't hold data. Mark entities we want to enable/disable
+// Only entities with this component will respond to clicks to disable/enable
 #[derive(Component)]
 struct DisableOnClick;
 
+// define the observer function
 fn disable_entities_on_click(
-    trigger: Trigger<Pointer<Click>>,
-    valid_query: Query<&DisableOnClick>,
-    mut commands: Commands,
+    trigger: Trigger<Pointer<Click>>, // event that contains which entity was clicked
+    valid_query: Query<&DisableOnClick>, // allows checking if the clicked entity has the component
+    mut commands: Commands,           // lets you modify entities (insert/remove)
 ) {
+    // get the clicked entity from the click event
     let clicked_entity = trigger.target();
 
+    // check if entity has component
     if valid_query.contains(clicked_entity) {
+        // 
         commands.entity(clicked_entity).insert(Disabled);
     }
 }
