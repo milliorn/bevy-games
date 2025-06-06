923ac7f777abce4c4c920ecf6dac09d52780071f
milliorn <scottmilliorn@gmail.com>
2025-06-06 11:02:12 -0700

    add: fps overlay

--- fps-overlay/src/main.rs
+++ fps-overlay/src/main.rs
@@ -1,3 +1,48 @@
+//! Showcase how to use and configure FPS overlay.
+
+use bevy::{
+    dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
+    prelude::*,
+    text::FontSmoothing,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            FpsOverlayPlugin {
+                config: FpsOverlayConfig {
+                    text_config: TextFont {
+                        // Here we define size of our overlay
+                        font_size: 42.0,
+                        // If we want, we can use a custom font
+                        font: default(),
+                        // We could also disable font smoothing,
+                        font_smoothing: FontSmoothing::default(),
+                        ..default()
+                    },
+                    // We can also change color of the overlay
+                    text_color: *TextColor::WHITE,
+                    // We can also set the refresh interval for the FPS counter
+                    refresh_interval: core::time::Duration::from_millis(100),
+                    enabled: true,
+                },
+            },
+        ))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // We need to spawn a camera (2d or 3d) to see the overlay
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(12.),
+            left: Val::Px(12.),
+            ..default()
+        },
+    ));
 }
