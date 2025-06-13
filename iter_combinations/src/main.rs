e4eec97d9b523d342d937f810bc8bf2d343865be
milliorn <scottmilliorn@gmail.com>
2025-06-13 05:24:54 -0700

    feat: Add BodyBundle to group components for body entities

--- iter_combinations/src/main.rs
+++ iter_combinations/src/main.rs
@@ -16,6 +16,17 @@ struct LastPos(Vec3);
 #[derive(Component)]
 struct Star;
 
+// groups common components for a body entity
+// bundle is used to group multiple components together
+#[derive(Bundle, Default)]
+struct BodyBundle {
+    mesh: Mesh3d, // tells bevy that this entity should be rendered as a 3d mesh
+    material: MeshMaterial3d<StandardMaterial>, // tells bevy how the 3d mesh should look
+    mass: Mass,
+    last_pos: LastPos,
+    acceleration: Acceleration,
+}
+
 // entry point
 fn main() {
     // start a minimal bevy ecs app
